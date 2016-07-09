e2d25887beed27fe5a7e06ae172e7e8c18f7f860
Adam Bourg <Adam.bourg@gmail.com>
2016-07-09 15:40:42 -0600

    Added a source command to reload the shell on major changes

--- build.sh
+++ build.sh
@@ -7,23 +7,36 @@ echo "--> Checking for Homebrew"
 # Check to see if we have homebrew, if not lets get it
 if ! type brew &> /dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  $(source ~/.bash_profile)
+  $(source ~/.zshrc)
 fi
 
 echo "--> Getting wget"
 # Homebrew install wget
 brew install wget
 
+$(source ~/.bash_profile)
+$(source ~/.zshrc)
+
 echo "--> Setting up git config"
 # Install git config
 cp dependencies/.gitconfig ~/
+
+$(source ~/.bash_profile)
+$(source ~/.zshrc)
 
 echo "--> Getting oh-my-zsh"
 # Install Oh My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+$(source ~/.bash_profile)
+$(source ~/.zshrc)
+
 echo "--> Installing Shiftit"
 # Install the window manager
 brew cask install shiftit
+$(source ~/.bash_profile)
+$(source ~/.zshrc)
 
 echo "--> Installing iterm"
 # Install Iterm2
@@ -50,6 +63,8 @@ else
   echo "--> Sublime: Creating SUBLIME CLI command"
   # Create Link for sublime cli
   ln -s  "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+  $(source ~/.bash_profile)
+  $(source ~/.zshrc)
 
   echo "--> Sublime: Adding package control"
   # Add Package Control
@@ -57,4 +72,15 @@ else
     cp -r dependencies/sublime/packages/* ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
   fi
 fi
+
+echo "--> Ruby: Installing ruby"
+$(brew install rbenv ruby-build)
+$(echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.bash_profile)
+$(echo 'if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi' >> ~/.zshrc)
+$(rbenv install 2.3.1)
+$(rbenv global 2.3.1)
+$(source ~/.bash_profile)
+$(source ~/.zshrc)
+echo "---> Ruby version: "
+ruby -v
 
